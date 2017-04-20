d9aa0029018c4ade34604e5878de63c0be42eb32
jackp0979 <jpomije14@gmail.com>
2017-04-20 08:56:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+ var currentTime = new Date();
+            var hours = currentTime.getHours();
+            var minutes = currentTime.getMinutes();
+          	var seconds = currentTime.getSeconds();
+            var suffix = "AM";
 
+            if (minutes < 10)
+                minutes = "0" + minutes;
+
+            if (hours >= 12) {
+                suffix = "PM";
+                hours = hours - 12;
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+
+            document.write("<b><div id='d1'>" + hours + ":" + 
+                minutes + ":" + seconds + " " + suffix + "</div></b>");
